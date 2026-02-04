6f5b236a615aa7f4ec4e64404a6e59c67baf9f77
VishSeran <vishwa.sovis@gmail.com>
2026-02-05 00:56:17 +0530

    App.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+function App (){
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div className='text-amber-600 font-bold flex-col bg-blue-950'>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  return(
+    <div className="flex flex-col pt-10 text-amber-50  bg-blue-950 max-h-screen ">
+      <h2 className="font-semibold text-center text-6xl">Hey Welcome to</h2>
+      <h1 className="font-bold text-center text-9xl">Cafe DeVMocha</h1>
+
+      <div className="pt-100 px-40">
+        <button className="border-blue-600 bg-green-500 font-medium p-3">Get Start</button>
       </div>
-      <h1 className='font-bold'>Vite + React</h1>
-      <div >
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p >
-        Click on the Vite and React logos to learn more
-      </p>
-      <button className="rounded-lg border border-transparent border-radius px-4 py-2 bg-amber-400 text-base font-medium hover:border">
-        Click me
-      </button>
-    </>
+    </div>
   )
 }
 
-export default App
+export default App;
