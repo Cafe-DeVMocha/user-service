17aab698bdd6343054d85797bcc775c32b45277d
VishSeran <vishwa.sovis@gmail.com>
2026-02-05 10:20:32 +0530

    Initalized header

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
+import Header from "./components/Header";
+
 function App (){
 
 
   return(
-    <div className="flex flex-col pt-10 text-amber-50  bg-blue-950 max-h-screen ">
-      <h2 className="font-semibold text-center text-6xl">Hey Welcome to</h2>
-      <h1 className="font-bold text-center text-9xl">Cafe DeVMocha</h1>
-
-      <div className="pt-100 px-40">
-        <button className="border-blue-600 bg-green-500 font-medium p-3">Get Start</button>
-      </div>
-    </div>
+    <Header />
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,35 @@
+export default function Header() {
+    return (
+
+        <div className=" bg-gray-900 text-gray-300">
+
+            <div className="flex items-center justify-between px-5 py-6 lg:px-16">
+                <div className="text-xl  font-bold">
+                    logo
+                </div>
+
+                <nav className="text-lg  md:flex sm:px-6 sm:gap-2 gap-6">  
+                    <a href="#home">Home</a>
+                    <a href="#About Us">About Us</a>
+                    <a href="#Menu">Menu</a>
+                    <a href="#Order">Order</a>
+                    <a href="#Contact Us">Contact Us</a>
+                </nav>
+                    
+               
+
+                <div className="hidden md:flex gap-4" >
+                    < a href="#">LinkedIn</a>
+                    < a href="#">LinkedIn</a>
+                    < a href="#">LinkedIn</a>
+                    < a href="#">LinkedIn</a>
+                </div>
+
+                <div className="sm:px-10 md:block">
+                    profile
+                </div>
+            </div>
+
+        </div>
+    )
+}
