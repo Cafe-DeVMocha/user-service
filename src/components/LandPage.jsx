20c4f1f9720e84f8937a72a6b907cb38a5208827
VishSeran <vishwa.sovis@gmail.com>
2026-02-09 22:07:39 +0530

    responsiveness updated of landpage

--- src/components/LandPage.jsx
+++ src/components/LandPage.jsx
@@ -4,23 +4,30 @@ import { Typewriter } from "react-simple-typewriter";
 export default function LandPage() {
 
     return (
-        <div id="home-section" className="bg-red-950 min-h-screen w-screen overflow-hidden">
-            <img
-                src={backgroundImg}
-                alt="Hero Background"
-                className="absolute inset-0 w-full h-full object-cover opacity-80"
-            />
+        
+        <div
+            id="home-section"
+            className="
+        relative
+        min-h-screen
+        w-full
+        bg-cover
+        bg-center
+        bg-no-repeat 
+      "
+            style={{ backgroundImage: `url(${backgroundImg})` }}
+        >
 
-            <div className="relative z-10 flex flex-row justify-between items-center ">
+            <div className="relative z-10 flex flex-row justify-between items-center bg-red-900/10 ">
 
-                <div className="flex lg:pt-30 lg:pl-30">
+                <div className="flex lg:pt-30 lg:pl-30 sm:pt-60 pt-40 pl-2">
 
-                    <div className="flex flex-col lg:gap-20">
-                        <div className=" flex flex-col text-white lg:gap-1">
-                            <h1 className="lg:text-8xl font-bold shadow-2xl">Where Every Bite,</h1>
-                            <h1  className="lg:text-8xl font-bold shadow-2xl">Matter</h1>
+                    <div className="flex flex-col lg:gap-10 sm:gap-0 gap-5 pt-35">
+                        <div className=" flex flex-col text-white lg:gap-1 gap-1">
+                            <h1 className="lg:text-8xl sm:text-8xl text-4xl font-bold shadow-2xl">Where Every Bite,</h1>
+                            <h1 className="lg:text-8xl sm:text-8xl text-4xl font-bold shadow-2xl">Matter</h1>
 
-                            <div className=" relative lg:mr-30 lg:mt-5 italic tracking-widest lg:text-xl font-mono text-white h-[70px] overflow-hidden">
+                            <div className=" relative lg:mr-30 lg:mt-5 italic tracking-widest lg:text-xl sm:text-xl text-sm font-mono text-white h-[70px] overflow-hidden">
                                 <Typewriter
                                     words={[
                                         "Experience a dining journey that celebrates flavor, sparks curiosity, and leaves every visit unforgettable."
@@ -36,7 +43,7 @@ export default function LandPage() {
                                 </Typewriter>
                             </div>
 
-                            <div className="relative h-[140px] lg:pr-130 lg:pt-10 italic tracking-widest lg:text-md text-white">
+                            <div className="relative h-[140px] lg:pr-130 lg:pt-10 pt-10 italic tracking-widest lg:text-[14px] sm:text-md text-xs text-justify pr-5 text-white">
                                 <Typewriter
                                     words={[
                                         "Step into a world where culinary artistry meets innovation. At DeVMocha, every dish is thoughtfully crafted from the finest ingredients, blending vibrant flavors, fresh textures, and contemporary presentation. From colorful gourmet salads to fusion mains, decadent desserts, and handcrafted drinks, each creation is designed to delight your senses. Whether you’re savoring a perfectly balanced brunch, enjoying a coffee crafted by expert baristas, or indulging in an exquisite dinner, every bite tells a story of passion, precision, and creativity. "
@@ -52,12 +59,12 @@ export default function LandPage() {
                                 </Typewriter>
                             </div>
 
-                             
+
 
                         </div>
 
-                        <div className=" flex flex-row text-white  lg:pt-10 lg:pb-3">
-                            <button className="bg-purple-800 lg:text-xl font-light lg:p-3 rounded-xl hover:scale-105 duration-10" >SignUp</button>
+                        <div className=" flex flex-row relative text-white md:justify-between lg:pt-10 lg:pb-10 pt-20 sm:pb-10">
+                            <button className="bg-purple-800 lg:text-xl font-normal md:p-4 p-3 md:w-md w-[150px] rounded-xl hover:scale-105 duration-10 hover:border-b-violet-300 hover:animate-pulse hover:font-bold" >SignUp</button>
                         </div>
                     </div>
 
