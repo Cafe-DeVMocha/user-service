17c65d89a5b5096a3b39e7a331bc64f72bd13797
VishSeran <vishwa.sovis@gmail.com>
2026-02-10 23:37:30 +0530

    add motions to menu card div

--- src/components/MenuCard.jsx
+++ src/components/MenuCard.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom"
 import sanwidtch from "../assets/sanwidtch.jpg"
 import { useState } from "react";
+import {easeIn, motion} from "framer-motion"
 
 
 export default function MenuCard({ imgUrl, title, description, pageUrl }) {
@@ -13,14 +14,19 @@ export default function MenuCard({ imgUrl, title, description, pageUrl }) {
 
 
     return (
-        <div id="menu-card" className="w-full max-w-md mx-auto hover:scale-102 duration-200 ">
+        <motion.div id="menu-card" className="w-full max-w-md mx-auto hover:scale-102 duration-200"
+            initial={{opacity: 0,y:50}}
+            whileInView={{opacity:1 ,y:0}}
+            viewport={{once:false, amount:0.1}}
+            transition={{duration:0.5, ease:easeIn}}
+        >
             <div className="flex flex-col p-4 sm:p-6 lg:p-8 bg-gray-100 rounded-2xl shadow-md ">
                 <div className="w-full">
                     <img src={imgUrl} className="w-full aspect-square object-cover rounded-2xl" />
                 </div>
 
                 <div className="mt-2">
-                    <p className="text-xl sm:text-2xl text-center lg:text-3xl font-semibold ">{title}</p>
+                    <p className="text-xl tracking-widest sm:text-2xl text-center lg:text-3xl font-semibold ">{title}</p>
                 </div>
 
                 <div className="mt-5">
@@ -31,12 +37,12 @@ export default function MenuCard({ imgUrl, title, description, pageUrl }) {
 
                 <div className="mt-5">
                     <Link to={url}>
-                        <button className="w-20 h-10 rounded-xl  bg-blue-700 hover:bg-blue-600 hover:duration-100
+                        <button className="w-25 h-12 text-xl font-serif tracking-wide  rounded-xl  bg-blue-700 hover:bg-blue-600 hover:duration-100
                         active:scale-95 duration-100 text-md text-white" onClick={handleClick}>Order</button>
                     </Link>
 
                 </div>
             </div>
-        </div>
+        </motion.div>
     )
 }
