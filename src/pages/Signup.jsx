9e4f6c19601ab121316c987431bafb834d361ac8
VishSeran <vishwa.sovis@gmail.com>
2026-02-14 23:11:26 +0530

    background darked signup page updated

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,6 +1,6 @@
 import { Alert } from "@mui/material";
 import { useState } from "react"
-import coverImg from "../assets/aboutus2.jpg"
+import coverImg from "../assets/aboutus3.jpg"
 
 
 export default function SignUp() {
@@ -43,10 +43,10 @@ export default function SignUp() {
 
 
                 <div className="text-white">
-                    background
+                    <img src ={coverImg}  className="overflow-hidden min-w-screen h-screen blur-md" />
                 </div>
 
-                <div className="flex flex-col justify-center text-white  items-center  bg-linear-to-br from-gray-800 via-gray-900 to-gray-950 opacity-90 shadow-2xl rounded-2xl m-5 hover:scale-102 duration-600">
+                <div className="flex flex-col justify-center text-white  items-center  bg-linear-to-br from-gray-800 via-gray-900 to-gray-950 opacity-90 shadow-2xl rounded-2xl m-5 ">
 
                     <p className="text-4xl font-semibold font-sans tracking-wide">Create an account</p>
 
@@ -56,30 +56,30 @@ export default function SignUp() {
 
                         <div className="flex flex-col gap-2 p-2  font-sans">
                             <label htmlFor="name" className="md:text-md text-gray-100 tracking-wider animate-floatBlob ">Name</label>
-                            <input className="outline-2 outline-gray-200 rounded-xl p-2 w-100 hover:outline-gray-300 text-gray-100 "
+                            <input className="outline-2 outline-gray-500 rounded-xl p-2 w-100 hover:outline-gray-300 text-gray-100 hover:scale-102 duration-600"
                                 id="name" name="name" type="text" required value={formData.name} onChange={handleChange} />
                         </div>
 
                         <div className="flex flex-col gap-2 p-2  font-sans">
                             <label htmlFor="email" className="md:text-md text-gray-100 tracking-wider">Email</label>
-                            <input className="outline-2 outline-gray-200 rounded-xl p-2 w-100 hover:outline-gray-300 text-gray-100 "
+                            <input className="outline-2 outline-gray-500 rounded-xl p-2 w-100 hover:outline-gray-300 text-gray-100 hover:scale-102 duration-600"
                                 id="email" required name="email" type="email" value={formData.email} onChange={handleChange} />
                         </div>
 
                         <div className="flex flex-col gap-2 p-2  font-sans">
                             <label htmlFor="password" className="md:text-md text-gray-100 tracking-wider">Password</label>
-                            <input className="outline-2 outline-gray-200 rounded-xl p-2 w-100 hover:outline-gray-300 text-gray-100 "
+                            <input className="outline-2 outline-gray-500 rounded-xl p-2 w-100 hover:outline-gray-300 text-gray-100 hover:scale-102 duration-600"
                                 id="password" required name="password" type="password" value={formData.password} onChange={handleChange} />
                         </div>
 
                         <div className="flex flex-col gap-2 p-2  font-sans">
                             <label htmlFor="confirmPassword" className="md:text-md text-gray-100 tracking-wider">Confirm Password</label>
-                            <input className="outline-2 outline-gray-200 rounded-xl p-2 w-100 hover:outline-gray-300 text-gray-100 "
+                            <input className="outline-2 outline-gray-500 rounded-xl p-2 w-100 hover:outline-gray-300 text-gray-100 hover:scale-102 duration-600"
                                 id="confirmPassword" required name="confirmPassword" minLength={8} type="password" value={formData.confirmPassword} onChange={handleChange} />
                         </div>
 
-                        <div className="">
-                            <button className=" text-gray-100 p-3 w-xs bg-green-600 rounded-xl hover:bg-green-500 duration-200 active:scale-95" type="submit">Submit</button>
+                        <div className="pt-8">
+                            <button className=" text-gray-100 p-3 w-3xs bg-green-600 rounded-xl hover:bg-green-500 duration-200 active:scale-95" type="submit">Submit</button>
                         </div>
                     </form>
 
