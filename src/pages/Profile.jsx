8a04d0c05f9d520895d800e9fe5de46da21a2cb2
VishSeran <vishwa.sovis@gmail.com>
2026-02-25 23:58:27 +0530

    user form top details updated

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -8,7 +8,7 @@ export default function Profile() {
     const { user } = useUser();
     const now = new Date();
     const month = now.toLocaleString('default', { month: 'long' });
-    const placeHolder = <SearchIcon fontSize='medium'/>
+    const placeHolder = <SearchIcon fontSize='medium' />
 
     return (
         <div className="flex">
@@ -26,25 +26,44 @@ export default function Profile() {
 
                     <div className="flex gap-4 items-center">
                         <div className="relative">
-                            <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2"/>
-                            <input type="text" id="search" placeholder= "Search" className="outline-1 px-10 bg-gray-50 opacity-70 h-8 w-70 rounded-md hover:outline-gray-200 active: outline-gray-300"/>
+                            <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2" />
+                            <input type="text" id="search" placeholder="Search" className="outline-1 px-10 bg-gray-50 opacity-70 h-8 w-70 rounded-md hover:outline-gray-200 active: outline-gray-300" />
                         </div>
-                        
+
                         <div>
-                            <img src={user.profilePic} className="w-15 h-15 object-cover rounded-full"/>
+                            <img src={user.profilePic} className="w-10 h-10 object-cover rounded-full" />
                         </div>
                     </div>
                 </div>
 
-                <div>
+                <div className="flex flex-col my-25 mx-5">
+                    <div className="">
 
+                    </div>
+
+                    <div className="flex justify-between items-center mx-5">
+                        <div className="flex gap-5">
+                            <div>
+                                <img src={user.profilePic} className="w-30 h-30 object-cover rounded-full" />
+                            </div>
+
+                            <div className="flex flex-col gap-1 justify-center">
+                                <p className="text-2xl font-medium">{user.name}</p>
+                                <p className="text-gray-600">{user.email}</p>
+                            </div>
+                        </div>
+                        <div>
+                            <button className="bg-blue-700 text-white py-2 px-6 rounded-md font-medium
+                            hover:bg-blue-600 hover:scale-105 hover:font-medium duration-150 active:bg-black active:font-medium active:scale-95"> Edit</button>
+                        </div>
+                    </div>
                 </div>
 
 
-              
 
 
-                
+
+
             </div>
         </div>
     )
