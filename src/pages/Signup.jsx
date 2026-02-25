8736f8f33887bcadb9dc52c0b40b4710c20ca44b
VishSeran <vishwa.sovis@gmail.com>
2026-02-25 20:08:41 +0530

    signup page updated. responsive updated

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -41,19 +41,19 @@ export default function SignUp() {
 
         <div>
 
-            <div className="relative  grid grid-cols-2 min-h-screen bg-linear-to-br from-gray-900 via-gray-900 to-gray-950">
+            <div className="relative md:grid grid-cols-2 min-h-screen bg-linear-to-br from-gray-900 via-gray-900 to-gray-950">
 
                 <div
-                    className="absolute inset-0 bg-center bg-cover blur-sm"
+                    className="absolute inset-0 bg-center bg-cover blur-sm rounded-2xl shadow-2xl"
                     style={{ backgroundImage: `url(${coverImg})` }}
                 >
                 </div>
 
                 <div className="relative text-white flex flex-col items-center px-4 md:px-8 py-4 md:py-60 ">
                     <div className="flex flex-col gap-4 ">
-                        <p className="text-7xl">Create Your <br /> Free Account </p>
+                        <p className="md:text-7xl text-5xl shadow-2xl">Create Your <br /> Free Account </p>
 
-                        <p className="text-2xl text-gray-200 opacity-85">Grab your offers on time, Share with your firends</p>
+                        <p className=" shadow-2xs text-xl md:text-2xl text-gray-200 opacity-85">Grab your offers on time, Share with your firends</p>
 
                     </div>
                 </div>
@@ -69,25 +69,25 @@ export default function SignUp() {
 
                             <div className="flex flex-col gap-2 p-2  font-sans">
                                 <label htmlFor="name" className="md:text-md text-gray-900 tracking-wider animate-floatBlob ">Name</label>
-                                <input className="outline-2 outline-gray-500 rounded-xl p-2 w-100 hover:outline-gray-300 text-gray-800 hover:scale-102 duration-600"
+                                <input className="outline-2 outline-gray-500 rounded-xl p-2 md:w-100 w-full max-w-md px-20  hover:outline-gray-300 text-gray-800 hover:scale-102 duration-600"
                                     id="name" name="name" type="text" required value={formData.name} onChange={handleChange} />
                             </div>
 
                             <div className="flex flex-col gap-2 p-2  font-sans">
                                 <label htmlFor="email" className="md:text-md text-gray-900 tracking-wider">Email</label>
-                                <input className="outline-2 outline-gray-500 rounded-xl p-2 w-100 hover:outline-gray-300 text-gray-800 hover:scale-102 duration-600"
+                                <input className="outline-2 outline-gray-500 rounded-xl p-2 md:w-100 w-full max-w-md px-20  hover:outline-gray-300 text-gray-800 hover:scale-102 duration-600"
                                     id="email" required name="email" type="email" value={formData.email} onChange={handleChange} />
                             </div>
 
                             <div className="flex flex-col gap-2 p-2  font-sans">
                                 <label htmlFor="password" className="md:text-md text-gray-900 tracking-wider">Password</label>
-                                <input className="outline-2 outline-gray-500 rounded-xl p-2 w-100 hover:outline-gray-300 text-gray-800 hover:scale-102 duration-600"
+                                <input className="outline-2 outline-gray-500 rounded-xl p-2 md:w-100 w-full max-w-md px-20  hover:outline-gray-300 text-gray-800 hover:scale-102 duration-600"
                                     id="password" required name="password" type="password" value={formData.password} onChange={handleChange} />
                             </div>
 
                             <div className="flex flex-col gap-2 p-2  font-sans">
                                 <label htmlFor="confirmPassword" className="md:text-md text-gray-900 tracking-wider">Confirm Password</label>
-                                <input className="outline-2 outline-gray-500 rounded-xl p-2 w-100 hover:outline-gray-300 text-gray-800 hover:scale-102 duration-600"
+                                <input className="outline-2 outline-gray-500 rounded-xl p-2 md:w-100 w-full max-w-md px-20  hover:outline-gray-300 text-gray-800 hover:scale-102 duration-600"
                                     id="confirmPassword" required name="confirmPassword" minLength={8} type="password" value={formData.confirmPassword} onChange={handleChange} />
                             </div>
 
