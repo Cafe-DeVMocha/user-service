bb6fed3161dc2613f829f4e024deac0ab76ee93a
VishSeran <vishwa.sovis@gmail.com>
2026-02-25 21:07:40 +0530

    profile page picture updated

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,28 @@
+import Header from "../components/Header";
+import { useUser } from "../context/userContext"
 
 export default function Profile() {
 
+    const { user } = useUser();
+
     return (
         <div>
-            <p>This is profile</p>
+            <div className="grid grid-cols-[1fr_1fr] ">
+
+                <div className="bg-blue-900 flex flex-col">
+                    <div className="items-center justify-center mx-auto my-20 bg-blue-900">
+                        <div>
+                            <img src={user.profilePic} className="h-50 w-50 rounded-full object-cover" />
+                        </div>
+
+                    </div>
+                </div>
+
+
+                <div>
+                    <p>Settings</p>
+                </div>
+            </div>
         </div>
     )
 }
